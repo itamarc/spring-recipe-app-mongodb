b33acd8a2fb92c0aabf50db3a476e2991581ac9b
Itamar Carvalho <itamarc@gmail.com>
2021-12-07 23:22:29 -0300

    Created project in Spring Initializr

---
+++ src/main/java/guru/springframework/springrecipeapp/SpringRecipeApp.java
@@ -0,0 +1,13 @@
+package guru.springframework.springrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipeApp.class, args);
+	}
+
+}
