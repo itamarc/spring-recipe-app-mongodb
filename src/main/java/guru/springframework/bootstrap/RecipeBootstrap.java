b6115df4acf8ee7f3cb5afeea48bb67cf336ef81
Itamar Carvalho <itamarc@gmail.com>
2022-01-13 14:12:03 -0300

    Refactoring to use Lombok

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -4,6 +4,7 @@ import guru.springframework.domain.*;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -31,11 +33,11 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
-        System.out.println("Loading Bootstrap Data");
+        log.debug("Bootstrap Data loaded");
     }
 
     private List<Recipe> getRecipes() {
-
+        log.debug("Getting recipes on bootstrap");
         List<Recipe> recipes = new ArrayList<>(2);
 
         //get UOMs
