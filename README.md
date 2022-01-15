6b0961d74d36dbdb6bfd0df8c3707b16bd535aeb
Itamar Carvalho <itamarc@gmail.com>
2022-01-15 11:46:26 -0300

    Implemented Recipe show page
    Also included CircleCI badge on README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # spring-recipe-app
-Recipe web app with Spring MVC
+
+[![CircleCI](https://circleci.com/gh/itamarc/spring-recipe-app/tree/master.svg?style=svg)](https://circleci.com/gh/itamarc/spring-recipe-app/tree/master)
+
+Recipe web app with Spring MVC.
+
+Part of the Spring Framework Guru course.
+
---
+++ src/test/java/guru/springframework/ctrl/RecipeControllerTest.java
@@ -0,0 +1,51 @@
+package guru.springframework.ctrl;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    @InjectMocks
+    RecipeController controller;
+
+    AutoCloseable closeable;
+
+    @BeforeEach
+    void setUp() {
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    void showById() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}
