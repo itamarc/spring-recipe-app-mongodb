3b16aa2371a08a6281047cfcadd5ddc6d1e74c82
Itamar Carvalho <itamarc@gmail.com>
2022-01-25 20:13:02 -0300

    Implemented view ingredients
    Lesson 202

---
+++ src/test/java/guru/springframework/ctrl/IngredientControllerTest.java
@@ -0,0 +1,55 @@
+package guru.springframework.ctrl;
+
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.services.RecipeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.anyLong;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class IngredientControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController controller;
+
+    MockMvc mockMvc;
+
+    AutoCloseable closeable;
+
+    @BeforeEach
+    void setUp() {
+        closeable = MockitoAnnotations.openMocks(this);
+        controller = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    void listIngredients() throws Exception {
+        // given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        // when
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        // then
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+}
