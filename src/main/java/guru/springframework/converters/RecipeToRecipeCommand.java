8db01ce59e9101d4daea3a6d5c32c2c999f0a445
Itamar Carvalho <itamarc@gmail.com>
2022-02-02 21:43:16 -0300

    Showing image from DB
    Lesson 212

--- src/main/java/guru/springframework/converters/RecipeToRecipeCommand.java
+++ src/main/java/guru/springframework/converters/RecipeToRecipeCommand.java
@@ -34,6 +34,7 @@ public class RecipeToRecipeCommand implements Converter<Recipe, RecipeCommand> {
         recipeCommand.setServings(source.getServings());
         recipeCommand.setSource(source.getSource());
         recipeCommand.setUrl(source.getUrl());
+        recipeCommand.setImage(source.getImage());
         if (source.getNotes() != null) {
             recipeCommand.setNotes(notesConverter.convert(source.getNotes()));
         }
--- src/main/java/guru/springframework/ctrl/ImageController.java
+++ src/main/java/guru/springframework/ctrl/ImageController.java
@@ -1,7 +1,9 @@
 package guru.springframework.ctrl;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.ImageService;
 import guru.springframework.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 /*
  * Created by itamar at 2022-02-02
@@ -35,5 +41,22 @@ public class ImageController {
         imageService.saveImageFile(Long.valueOf(id), file);
         
         return "redirect:/recipe/" + id + "/show";
+    }
+
+    @GetMapping("recipe/{id}/recipeimage")
+    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws Exception {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+
+        byte[] byteArray = new byte[recipeCommand.getImage().length];
+
+        int i = 0;
+
+        for (Byte boxedByte : recipeCommand.getImage()) {
+            byteArray[i++] = boxedByte; // auto unboxing
+        }
+
+        response.setContentType("image/jpeg");
+        InputStream is = new ByteArrayInputStream(byteArray);
+        IOUtils.copy(is, response.getOutputStream());
     }
 }
