cfe3cb31abb23da6feea03d7c001514a08638a85
Itamar Carvalho <itamarc@gmail.com>
2022-02-02 21:09:33 -0300

    Saving image file to database
    Lesson 211

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,8 +1,13 @@
 package guru.springframework.services;
 
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /*
  * Created by itamar at 2022-02-02
@@ -10,9 +15,33 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        // TODO implement code to save file
-        log.debug("Received file: " + file.getOriginalFilename());
+        log.debug("Saving file: " + file.getOriginalFilename());
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            log.error("Error occurred: " + e);
+            e.printStackTrace();
+        }
     }
 }
