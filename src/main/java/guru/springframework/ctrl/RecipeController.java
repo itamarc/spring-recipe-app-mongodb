0229df11dd0be8f06129de261c801ee2c1ecbbaf
Itamar Carvalho <itamarc@gmail.com>
2022-02-13 17:43:31 -0300

    Showing error data in 404 error page

--- src/main/java/guru/springframework/ctrl/RecipeController.java
+++ src/main/java/guru/springframework/ctrl/RecipeController.java
@@ -55,10 +55,12 @@ public class RecipeController {
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ModelAndView handleNotFound() {
+    public ModelAndView handleNotFound(Exception exception) {
         log.error("Handling not found exception");
+        log.error(exception.getMessage());
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
         return modelAndView;
     }
 }
